c0640bd79ef33ced8b85d36b58c11343314466ac
iamtonmoy0 <edith@localhost.localdomain>
2022-12-24 08:29:37 +0600

    routes added

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,7 +6,10 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("Post")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("Get")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("Get")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
 }
