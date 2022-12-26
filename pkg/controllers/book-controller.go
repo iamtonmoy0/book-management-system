d24555b8d2a60c2d990eb79fb391caeef87df1c2
iamtonmoy0 <edith@localhost.localdomain>
2022-12-26 14:13:24 +0600

    controllers  added

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"book-management-system/pkg/models"
+	"encoding/json"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/iamtonmoy0/book-management-system/pkg/models"
+	"github.com/iamtonmoy0/book-management-system/pkg/utils"
+)
+
+var NewBook models.Book
+func getBook(w http.ResponseWriter , r *http.Request){
+	newBooks:= models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+func GetBookById(w http.ResponseWriter ,r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID ,err := strconv.ParseInt(bookId,0,0)
+	if err != nil{
+		fmt.Println("error while parsing")
+	}
+	bookDetails, := models.GetBookById(ID)
+	res, :=json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+func CreateBook (w http.ResponseWriter , r *http.Request){
+
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b:=CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
