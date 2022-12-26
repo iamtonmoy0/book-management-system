c777da5c238bb2071806616089e903759208798d
iamtonmoy0 <edith@localhost.localdomain>
2022-12-26 15:13:56 +0600

    update func  added

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -34,7 +34,7 @@ func GetBookById(w http.ResponseWriter ,r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
-func CreateBook (w http.ResponseWriter , r *http.Request){
+func CreateBook(w http.ResponseWriter , r *http.Request){
 
 	CreateBook := &models.Book{}
 	utils.ParseBody(r, CreateBook)
@@ -43,3 +43,44 @@ func CreateBook (w http.ResponseWriter , r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter ,r * http.Request){
+	vars := mux.Vars(r)
+	bookId := Vars["bookId"]
+	ID ,err := strconv.ParseInt(bookId, 0,0)
+	if err != nil{
+		fmt.Println("error while parsing")
+
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type" ,"pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+
+func UpdateBook(w http.ResponseWriter ,r * http.Request){
+
+
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars["bookId"]
+	Id , err := strconv.ParseInt(bookId ,0,0)
+	if err != nil{
+		fmt.Println("error while parsing")
+	}
+	bookDetails , bd :=models.GetBookById(ID)
+	if updateBook.Name != ""{
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != ""{
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != ""{
+		bookDetails.Publication = updateBook.Publication
+	}
+}
+
+
